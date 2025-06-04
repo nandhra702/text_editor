966c2279af3f0812746acaa7e0a9ef089b58cf98
Sukhraj Singh <145795038+nandhra702@users.noreply.github.com>
2025-06-04 12:40:07 +0530

    Chapter_2_ends

--- kilo.cpp
+++ kilo.cpp
@@ -1,41 +1,81 @@
+/*** INCLUDES ***/
+
 #include<ctype.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 #include<iostream>
+#include<error.h>
 using namespace std;
 
+/*** DATA ***/
 struct termios orig_termios;
 
+/*** TERMINAL ***/
+
+//a die function that prints the error message and exits. Most C library functions that fail will set the global errno variable to indicate what the error was. perror() looks at the global errno variable and prints a descriptive error message for it. Exit with status 1 means failure, with 0 means success.
+
+void die( const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+        die("tcsetattr");
+    }
 }
 
 //traditionally, our terminal works in canonical mode. It interprets certain keystrokes in a particular way(like upward //arrow). We need to disable that.So we enable raw mode so that all keystrokes go straight to our program without preprocessing. //we want to clear the echo bit right, so thats what we are doing.
 
-void enableRawMode(){
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode); //at_exit
-    struct termios raw = orig_termios;
-    tcgetattr(STDIN_FILENO,&raw);
+void enableRawMode() {
+  if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
+    die("tcgetattr");
+  }
 
-    raw.c_lflag &= ~(ECHO | ICANON); 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  atexit(disableRawMode); //no braces as atexit takes a fxn pointer (add braces itself)
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  
+  
 
+  //this is basically minimum time before which read function shall return
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
+    die("tcsetattr");
+  }
 }
+
+/*** INIT ***/
 
 int main(){
 
     enableRawMode();
 
-    char c;
-    while(read(STDIN_FILENO, &c,1)==1 && c !='q');{
+    //iscntrl() tests whether a character is a control character. Control characters are nonprintable characters that we don’t want to print to the screen
+
+    while(1){
+        char c = '\0';
+        if(read(STDIN_FILENO, &c,1) == -1 && errno!= EAGAIN){
+            die("read");
+        }
+
         if(iscntrl(c)){
-            cout<<c<<endl;
+           // cout<<int(c)<<endl;
+           printf("%d\r\n", c);
         }
         else{
-            cout<<"("<<to_int(c)<<")";
+             //cout << c << " (" << (int)c << ")" << endl;
+             printf("%d ('%c')\r\n", c, c);
+        }
+        if(c=='q'){
+            break;
         }
     }
     return 0;
